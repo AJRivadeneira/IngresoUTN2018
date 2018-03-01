f626e9cad4a0faeb94e316351b00cf851bf6dbe1
AJRivadeneira <36230536+AJRivadeneira@users.noreply.github.com>
2018-03-01 12:24:44 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -4,13 +4,40 @@ function Mostrar()
     var nota;
     var promedio;
     var notaBaja;
-    var f;
-    var m;
-    var mNotaAlta;
+    var acumNotas = 0;
+    var varArriba6 = 0;
+    var flag = 0;
 	
-    for(var alumnos = 0;alumnos <= 6; alumnos++)
+    for(var i = 0;i < 6; i++)
     {
         nota = parseInt(prompt("Ingrese nota"));
-    }
+
+        while (nota < 0 || nota >10)
+        {
+            nota = parseInt(prompt("Error. Reingrese una nota entre el 0 al 10"));
+        }
+
+        sexo = prompt("Ingrese sexo");
+
+        while (sexo != "f" && sexo != "m")
+        {
+            sexo = prompt("Error. Reingrese un sexo correcto (f o m)");
+        }
+        acumNotas += nota;
+
+        if (nota < notaBaja || flag == 0)
+        {
+            notaBaja = nota;
+            flag = 1;
+        }
+
+        if (sexo == "m" && nota >= 6)
+        {
+            varArriba6++;
+        }
+
+        promedio = acumNotas / 6
+    }   
+    alert("El promedio de las notas es " + promedio.toFixed(2) + " \nMientras la nota mas baja fue " + notaBaja + " \nTambien " + varArriba6 + " varones tuvieron entre 6 o mas de nota...")
 }
 
